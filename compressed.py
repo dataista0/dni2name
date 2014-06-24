ec907f59905d922aff55a919c2ae648068fba486
julian3833 <julianpeller@gmail.com>
2014-06-23 22:26:27 -0300

    recompress

--- compressed.py
+++ compressed.py
@@ -2,28 +2,22 @@
 import sys
 from GoogleSearchAPI.google import Google
 class dni2name:
-	def checkDNI(self, id):
-		results = Google.search("DNI "+str(id))
-		name = self.findName(results)
-
-		if name:
-			print id, name
-		return name
-	def checkRange(self, min, max):
-		return [self.checkDNI(id) for id in range(int(min), int(max))]
-	def extractName(self, result):
-		if "www.dateas.com" in result.link:
-			return result.name[0:result.name.find(" -")]
-		
-		if "buscardatos.com" in result.link:
-			return result.name[0:result.name.find(",")]
-		return False
-	def findName(self, google_results):
+	def checkDNI(self, id):	
+		google_results = Google.search("DNI "+str(id))
 		for result in google_results:
 			name = self.extractName(result)
 			if name is not False:
+				print id, name
 				return name
 		return False
+	def extractName(self, result):
+		if "buscardatos.com" in result.link:
+			return result.name[0:result.name.find(",")]
+		if "www.dateas.com" in result.link:
+			return result.name[0:result.name.find(" -")]
+		return False
+	def checkRange(self, min, max):
+		return [self.checkDNI(id) for id in range(int(min), int(max))]
 	def runExample(self):
 		self.checkDNI("33779884") #Loi's ID number
 if __name__ == "__main__":
